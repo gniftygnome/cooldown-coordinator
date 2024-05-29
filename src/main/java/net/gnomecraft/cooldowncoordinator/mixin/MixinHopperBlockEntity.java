692c0e6453b9520318a69d6100ba2f9a5fc85ce6
gniftygnome <minecraft@gnifty.net>
2024-05-29 13:47:00 -0700

    Fix repeated transfers to Fabric Transfer API-only modded blocks.
    - Fix repeated transfers to Fabric Transfer API-only modded blocks
    * Resolves #2

--- src/main/java/net/gnomecraft/cooldowncoordinator/mixin/MixinHopperBlockEntity.java
+++ src/main/java/net/gnomecraft/cooldowncoordinator/mixin/MixinHopperBlockEntity.java
@@ -122,6 +122,7 @@ public abstract class MixinHopperBlockEntity extends LootableContainerBlockEntit
                 }
 
                 cir.setReturnValue(true);
+                return;
             }
         }
 
